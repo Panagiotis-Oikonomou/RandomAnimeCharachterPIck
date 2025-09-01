ee9d5133a1fb60cb446311b55fe60e85b57918bb
ΠΑΝΑΓΙΩΤΗΣ ΟΙΚΟΝΟΜΟΥ <dit22153@office365.uop.gr>
2025-09-01 19:36:34 +0300

    modified the images

--- imggenerator.js
+++ imggenerator.js
@@ -6,18 +6,18 @@ const imagesC = ['img/C/Charmi.jpg', 'img/C/Chopper.jpg', 'img/C/Crocodile.jpg']
 const imagesD = ['img/D/Diablo.jpg'];
 const imagesE = [];
 const imagesF = ['img/F/Feitan.jpg', 'img/F/Fern.jpg'];
-const imagesG = ['img/G/Gen.jpg', 'img/G/Gon.jpg', 'img/G/Gabimaru.jpg', 'img/G/Giyuu.jpg'];
+const imagesG = ['img/G/Gen.jpg', 'img/G/Gon.jpg', 'img/G/Gabimaru.jpg', 'img/G/Giyuu.jpg', 'img/G/Gobta.jpg'];
 const imagesH = ['img/H/Hairo.jpg', 'img/H/Heisuke.jpg', 'img/H/Heiten.jpg', 'img/H/Hyouga.jpg', 'img/H/Hoshina.jpg', 'img/H/Himmel.jpg', 'img/H/Hibana.jpg', 'img/H/Hanma.jpg'];
 const imagesI = ['img/I/Itachi.jpg'];
 const imagesJ = ['img/J/Jinbei.jpg', 'img/J/Jiraiya.jpg'];
 const imagesK = ['img/K/Kaidou.jpg', 'img/K/Kakashi.jpg', 'img/K/Kenma.jpg', 'img/K/Komamura.jpg', 'img/K/Khun.jpg', 'img/K/Kaneki.jpg', 'img/K/Kikoru.jpg', 'img/K/Kaldo.jpg', 'img/K/Kazuma.jpg', 'img/K/Kuboyasu.jpg', 'img/K/Kilua.jpg', 'img/K/Koby.jpg', 'img/K/Katakuri.jpg', 'img/K/Kid.jpg'];
 const imagesL = ['img/L/Lance.jpg', 'img/L/Lero ro.jpg', 'img/L/Law.jpg', 'img/L/Luna.jpg'];
-const imagesM = ['img/M/Mash.jpg', 'img/M/Minato.jpg', 'img/M/Mina Ashiro.jpg', 'img/M/Macaron.jpg', , 'img/M/Mob.jpg', 'img/M/Mitsuri.jpg'];
+const imagesM = ['img/M/Mash.jpg', 'img/M/Minato.jpg', 'img/M/Mina Ashiro.jpg', 'img/M/Macaron.jpg', 'img/M/Mob.jpg', 'img/M/Mitsuri.jpg'];
 const imagesN = ['img/N/Nami.jpg', 'img/N/Noelle.jpg', 'img/N/Nagisha.jpg', 'img/N/Nendou.jpg'];
 const imagesO = ['img/O/Obanai.jpg'];
 const imagesP = ['img/P/Pain.jpg', 'img/P/Perona.jpg', 'img/P/Prince.jpg'];
 const imagesQ = [];
-const imagesR = ['img/R/Rukia.jpg', 'img/R/Regen.jpg', 'img/R/Reo.jpg', 'img/R/Robin.jpg'];
+const imagesR = ['img/R/Rukia.jpg', 'img/R/Regen.jpg', 'img/R/Reo.jpg', 'img/R/Robin.jpg', 'img/R/Rimuru.jpg'];
 const imagesS = ['img/S/Saiki.jpg','img/S/Saitama.jpg', 'img/S/Sanji.jpg', 'img/S/Senku.jpg', 'img/S/Shisui.jpg', 'img/S/Sakamoto.jpg', 'img/S/Shiba hakai.jpg', 'img/S/Shin.jpg', 'img/S/Shinobu kotsou.jpg', 'img/S/Shishiba.jpg', 'img/S/Smoker.jpg', 'img/S/Sanemi.jpg', 'img/S/Suika.jpg', 'img/S/Suga.jpg'];
 const imagesT = ['img/T/Tobi.jpg', 'img/T/Tobirama.jpg', 'img/T/Tokoyami.jpg', 'img/T/Toritsuka.jpg', 'img/T/Tanjiro.jpg', 'img/T/Toshiro.jpg'];
 const imagesU = ['img/U/Uta.jpg', 'img/U/Ussop.jpg'];
@@ -26,7 +26,6 @@ const imagesW = [];
 const imagesX = ['img/X/Xeno.jpg'];
 const imagesY = ['img/Y/Yu Ishigami.jpg' ,  'img/Y/Yamato.jpg'];
 const imagesZ = [];
-let images = [];
 let stopTime = 0;
 let interval;  
 
@@ -55,7 +54,7 @@ button.addEventListener('click', () => {
 
   let slct = document.getElementById("select").value;
     
-  stopTime = Math.floor(Math.random() * (5000 - 3000 + 1)) + 2000;
+  stopTime = Math.floor(Math.random() * (9000 - 3000 + 1)) + 2000;
 
   let imageSets = {
     A: imagesA,
@@ -94,6 +93,6 @@ button.addEventListener('click', () => {
   }
 
   //interval = setInterval(changeImage, 50);
-  interval = setInterval(changeImage, 100);
+  interval = setInterval(changeImage, 1000);
   setTimeout(stopChangingImages, stopTime);
 });
